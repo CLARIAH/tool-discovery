c547d5975d8cce1152785d64d3da00cf08d22799
Maarten van Gompel <proycon@anaproy.nl>
2022-03-24 21:07:58 +0100

    implemented support for turtle variant + fixes

--- bin/harvest.sh
+++ bin/harvest.sh
@@ -8,12 +8,15 @@ die() {
     exit 1
 }
 
+
+CSS="$BASEURI/resources/codemeta.css,$BASEURI/resources/fontawesome.css"
 if [ -n "$1" ]; then
     BASEURI="$1"
-    OPTS="--baseuri \"$1\""
+    HARVEST_OPTS="--baseuri $1"
 else
-    OPTS=""
+    HARVEST_OPTS=""
 fi
+CODEMETAPY_OPTS="--toolstore --css $CSS"
 
 CONFIGURL="$2"
 [ -n "$CONFIGURL" ] || die "No configuration URL provided (expected a git repository)"
@@ -40,8 +43,8 @@ mkdir -p /tmp/out
 
 
 #Run the harvester
-echo "Invoking harvester: codemeta-harvester $OPTS --outputdir /tmp/out /usr/src/source-registry/$CONFIGPATH && rsync --delete -av /tmp/out/ /tool-store-data/ " >&2
-if codemeta-harvester $OPTS --outputdir /tmp/out "/usr/src/source-registry/$CONFIGPATH"; then
+echo "Invoking harvester: codemeta-harvester $HARVEST_OPTS --opts \"$CODEMETAPY_OPTS\" --outputdir /tmp/out /usr/src/source-registry/$CONFIGPATH && rsync --delete -av /tmp/out/ /tool-store-data/ " >&2
+if codemeta-harvester $HARVEST_OPTS --opts "$CODEMETAPY_OPTS" --outputdir /tmp/out "/usr/src/source-registry/$CONFIGPATH"; then
 
     #(re-)copy CSS
     cp -R /usr/lib/python3.*/site-packages/codemeta/resources/ /tmp/out/
@@ -50,23 +53,29 @@ if codemeta-harvester $OPTS --outputdir /tmp/out "/usr/src/source-registry/$CONF
         cp -f /etc/css/*.css /tmp/out/resources/
     fi
 
-    #Creating HTML views
+    #Creating HTML views and Turtle variants
     for f in /tmp/out/*.codemeta.json; do
-        echo "Creating HTML view for $(basename "$f")...">&2
         if [ -e "$f" ]  && [ "$(basename "$f")" != "all.json" ]; then
+            echo "Creating HTML view for $(basename "$f")...">&2
             outfile=$(echo $f | sed -e 's/\.json$/.html/')
-            codemetapy $OPTS -o html --css "$BASEURI/codemeta.css" "$f" > "$outfile"
+            codemetapy $CODEMETAPY_OPTS -o html "$f" > "$outfile"
+            echo "Creating Turtle version for $(basename "$f")...">&2
+            outfile=$(echo $f | sed -e 's/\.json$/.ttl/')
+            codemetapy $CODEMETAPY_OPTS -o turtle "$f" > "$outfile"
         fi
     done
 
     #Creating joined graph
-    echo "Creating joined graph: codemetapy --graph /tmp/out/*.codemeta.json > /tmp/out/all.json">&2
-    codemetapy $OPTS --graph /tmp/out/*.codemeta.json > /tmp/out/all.json || die "failed to create joined graph"
+    echo "Creating joined graph (json)">&2
+    codemetapy $CODEMETAPY_OPTS --graph /tmp/out/*.codemeta.json > /tmp/out/all.json || die "failed to create joined graph"
+    echo "Creating joined graph (turtle)">&2
+    codemetapy $CODEMETAPY_OPTS -o turtle --graph /tmp/out/*.codemeta.json > /tmp/out/all.ttl || die "failed to create joined graph"
 
     #Creating HTML index
-    codemetapy $OPTS -o html --graph --css "$BASEURI/resources/codemeta.css,$BASEURI/resources/fontawesome.css" /tmp/out/*.codemeta.json > /tmp/out/index.html || die "failed to create HTML index"
+    echo "Creating HTML index">&2
+    codemetapy $CODEMETAPY_OPTS -o html --graph /tmp/out/*.codemeta.json > /tmp/out/index.html || die "failed to create HTML index"
 
-    echo "Syncing to target dir">&2
+    echo "Syncing temporary output to target dir">&2
     rsync --exclude 'archive' --delete -av /tmp/out/ /tool-store-data/ || die "failed to rsync"
 
     echo "Stopping the Tool Store API (will automatically restart)">&2
